5d75b1ccf495200e3f6680a4869b6268abb5ca16
kimy <kimysechao@gmail.com>
2026-04-07 16:28:06 +0200

    Exo 6

--- src/classesjava/Voiture.java
+++ src/classesjava/Voiture.java
@@ -17,13 +17,14 @@ public class Voiture {
     int annee;
     String carburant;
     double prix;
+    Pneu pneu;
 
-    public Voiture(String marque, String modele, int annee, double prix) {
+    public Voiture(String marque, String modele, int annee, double prix, Pneu pneu) {
         this.marque = marque;
         this.modele = modele;
         this.annee = annee;
         this.prix = prix;
+        this.pneu = pneu;
     }
 }
     }
-}
